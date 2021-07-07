52af14cb906944a269471a4a3491f29c7a3ef4ee
jjholt <jjholt@MPB>
2021-07-07 23:44:30 +0100

    Improvements to graph importing

--- matlab/example.m
+++ matlab/example.m
@@ -1,4 +1,7 @@
-figure,plot(x,y);
-xlabel(''); ylabel(''); axis square
-legend('', 'y=x', 'Location', 'best');
-print('eps/example.eps','-depsc')
+x=linspace(0,2*pi);
+y=sin(x);
+plot(x,y);
+xlabel('x'); ylabel('f(x)'); axis square
+legend('f(x)=sin(x)');
+print('figures/graph.eps','-depsc')
+matlab2tikz('figures/graph.tex', 'height', '\fheight', 'width', '\fwidth');
